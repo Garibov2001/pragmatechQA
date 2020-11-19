ef729c9c8a1e254eabc06956697927d09025b766
Garibov2001 <mahmood.garibov@gmail.com>
2020-11-20 02:29:22 +0400

    Aydan's bug is fixed.

--- student/urls.py
+++ student/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('tags/', views.tags, name='student-tags'),
     path('tags/index', views.tag_info, name='student-tags-index'),
     path('about/', views.about, name='student-about'),
-    path('rules/', views.rules, name='student-about'),
+    path('rules/', views.rules, name='student-rules'),
     path('create-topic/', views.page_create_topic, name='student_page_create_topic'),
 ]
