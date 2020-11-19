43f4037f2758f090a7d97d7477c772d064b5f663
Aydan Samedova <aydan2903@gmail.com>
2020-11-19 23:55:00 +0400

    create_post_page

--- student/urls.py
+++ student/urls.py
@@ -3,4 +3,5 @@ from student import views
 
 urlpatterns = [
     path('', views.home, name='student-home'),
+    path('create-topic/', views.page_create_topic, name='student_page_create_topic'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+
 def home(request):
     context={
     }
     return render(request, 'main_page/home.html', context)
+
+def page_create_topic(request):
+    context={
+    }
+    return render(request, 'main_page/page-create-topic.html', context)
