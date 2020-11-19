8ffe3778f6331c170a72d890f420f01f846147b1
Garibov2001 <mahmood.garibov@gmail.com>
2020-11-19 22:40:14 +0400

    Tags and each tag info page is done.

--- student/urls.py
+++ student/urls.py
@@ -3,4 +3,6 @@ from student import views
 
 urlpatterns = [
     path('', views.home, name='student-home'),
+    path('tags/', views.tags, name='student-tags'),
+    path('tags/index', views.tag_info, name='student-tags-index'),
 ]
--- student/views.py
+++ student/views.py
@@ -4,3 +4,17 @@ def home(request):
     context={
     }
     return render(request, 'main_page/home.html', context)
+
+def tags(request):
+    context={
+    }
+    return render(request, 'categories/tags.html', context)
+
+def tag_info(request):
+    context={
+    }
+    return render(request, 'categories/single_tag_info.html', context)
+
+def staff_and_users(request):
+    return render(request, 'categories/single_tag_info.html')
+
