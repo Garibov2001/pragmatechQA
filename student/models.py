0df5b26239bf416c6aa0945a986bcc8daa24079e
Aytac Huseynova <aytac11011@gmail.com>
2020-11-20 12:52:08 +0400

    FAQ table

--- student/models.py
+++ student/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Faq(models.Model):
+    title = models.CharField(max_length = 120, verbose_name = "Başlıq")
+    content = models.CharField(max_length = 200, verbose_name = "Məzmun")
