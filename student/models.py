e768514ee09f07349408973f8f5a44692946abce
Tarlan Soltanov <soltanov.terlan04@outlook.com>
2020-11-20 18:21:30 +0400

    Question and category tables created

--- student/models.py
+++ student/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class category(models.Model):
+    """Model definition for category."""
+
+    name = models.CharField(verbose_name=("Ad"), max_length=50)
+
+    description = models.TextField(verbose_name=("Açıqlama"))
+
+    class Meta:
+        """Meta definition for category."""
+
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        """Unicode representation of category."""
+        return self.name
+
+
+class question(models.Model):
+    """Model definition for question."""
+
+    title = models.CharField(verbose_name=("Başlıq"), max_length=50)
+    category = models.ManyToManyField("category", verbose_name=("Kategoriya"))
+    content = models.TextField(verbose_name=("Kontent"))
+
+
+    class Meta:
+        """Meta definition for question."""
+
+        verbose_name = 'question'
+        verbose_name_plural = 'questions'
+
+    def __str__(self):
+        """Unicode representation of question."""
+        return self.title
