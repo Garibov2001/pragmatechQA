dd8d0f98d0cf5cdac6f342604cab39cfb21c8ab6
Aytac Huseynova <aytac11011@gmail.com>
2020-11-21 17:11:02 +0400

    update page-tabs.html

--- student/models.py
+++ student/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Setting(models.Model):
+    communityRules = models.CharField(max_length = 255, name="cRules")
+
+
 class Faq(models.Model):
     title = models.CharField(max_length = 120, verbose_name = "Başlıq")
     content = models.CharField(max_length = 200, verbose_name = "Məzmun")
+
+
